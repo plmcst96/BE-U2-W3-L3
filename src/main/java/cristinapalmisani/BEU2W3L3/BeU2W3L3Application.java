e78eebf512e9dfcd55e339167a70182b3cb22132
Cristina Palmisani <cristina.palmisani@outlook.it>
2024-01-24 15:23:10 +0100

    Chain pattern

--- src/main/java/cristinapalmisani/BEU2W3L3/BeU2W3L3Application.java
+++ src/main/java/cristinapalmisani/BEU2W3L3/BeU2W3L3Application.java
@@ -1,6 +1,5 @@
 package cristinapalmisani.BEU2W3L3;
 
-import cristinapalmisani.BEU2W3L3.composite.CompositeRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
